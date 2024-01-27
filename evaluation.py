55d1a8a0b790d94c12650434ee08a032f0ae5faf
Ryan Caabay <ryanpcaabay9@gmail.com>
2024-01-27 22:25:14 +0800

    Added new modules

---
+++ evaluation.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from xgboost import plot_importance
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score 
+from model import saved_model
+
+
+data = pd.read_csv('reviews_preprocessed.csv')
+data = data.drop(['review'], axis='columns')
+
+X = data.drop(columns=['sentiment'])
+y = data['sentiment']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+
+y_pred = saved_model.predict(X_test)
+y_train_pred = saved_model.predict(X_train)
+
+print('\nTrain Accuracy Score: ', accuracy_score(y_train, y_train_pred))
+print('Test Accuracy Score: ', accuracy_score(y_test, y_pred,), '\n')
+
+print('Train Precision Score: ', precision_score(y_train, y_train_pred, average='weighted'))
+print('Test Precision Score: ', precision_score(y_test, y_pred, average='weighted'), '\n')
+
+print('Train Recall Score: ', recall_score(y_train, y_train_pred, average='weighted'))
+print('Test Recall Score: ', recall_score(y_test, y_pred, average='weighted'), '\n')
+
+print('Train F1 Score: ', f1_score(y_train, y_train_pred, average='weighted'))
+print('Test F1 Score: ', f1_score(y_test, y_pred, average='weighted'), '\n')
+
+score = cross_val_score(saved_model, X, y, cv=10)
+#print(score)
+print('Cross-Validation Score: ', score.mean(), '\n')
+
+plot_importance(saved_model)
+plt.show()
---
+++ model.py
@@ -0,0 +1,68 @@
+import pandas as pd
+from datetime import datetime
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+import joblib
+
+
+params={
+    "objective": ['multi:softmax'],
+    "learning_rate": [0.05, 0.10, 0.15, 0.20, 0.25, 0.30],
+    "max_depth": [3, 4, 5, 6, 8, 10, 12, 15],
+    "min_child_weight": [1, 3, 5, 7],
+    "gamma": [0.0, 0.1, 0.2 , 0.3, 0.4],
+    "colsample_bytree": [0.3, 0.4, 0.5 , 0.7]
+}
+
+
+def timer(start_time=None):
+    if not start_time:
+        start_time = datetime.now()
+        return start_time
+    elif start_time:
+        thour, temp_sec = divmod((datetime.now()-start_time).total_seconds(), 3600)
+        tmin, tsec = divmod(temp_sec, 60)
+        print('\nTime taken: %i hours %i minutes and %s seconds.\n' % (thour, tmin, round(tsec, 2)))
+
+
+data = pd.read_csv('reviews_preprocessed.csv')
+data = data.drop(['review'], axis='columns')
+
+
+X = data.drop(columns=['sentiment'])
+y = data['sentiment']
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+
+"""
+model = XGBClassifier()
+grid_search = GridSearchCV(model, param_grid=params, refit=True, scoring='roc_auc_ovr', n_jobs=-1, cv=5, verbose=3)
+
+start_time = timer(None)
+grid_search.fit(X_train, y_train)
+timer(start_time)
+
+#print(grid_search.best_estimator_)
+
+#best_params = grid_search.best_params_
+
+# Print the best parameters
+print(best_params, '\n')
+"""
+
+best_params = {
+    "colsample_bytree": 0.5, 
+    "gamma": 0.4, 
+    "learning_rate": 0.05, 
+    "max_depth": 4, 
+    "min_child_weight": 7, 
+    "objective": 'multi:softmax'
+}
+
+classifier = XGBClassifier(**best_params)
+classifier.fit(X_train, y_train)
+
+joblib.dump(classifier, 'xgb_model.pkl')
+saved_model = joblib.load('xgb_model.pkl')
+
